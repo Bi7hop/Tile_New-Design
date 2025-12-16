b5607eb5475171756edf463f498856a799009ca4
Marcel Menke <marcel.menke1981@gmail.com>
2025-12-16 09:56:27 +0100

    Add custom fonts stylesheet to admin login page
    Included a link to fonts.css in the admin login page to enable custom font usage. This enhances the visual appearance and consistency of the admin interface.

--- admin/index.php
+++ admin/index.php
@@ -79,6 +79,7 @@ $csrf_token = generate_csrf_token();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin-Login - Fliesen Runnebaum</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="../assets/css/fonts.css">
     <link rel="stylesheet" href="css/admin-style.css">
     <link rel="icon" href="../assets/img/fliesenrunnebaum_favicon.ico" type="image/x-icon">
     
